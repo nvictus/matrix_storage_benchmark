cfbf821ee766a29a7276b34f9857d5b37077b24e
Nezar Abdennur <nabdennur@gmail.com>
2016-08-08 08:53:42 -0400

    Add bench results

--- scripts/cooler_binned.py
+++ scripts/cooler_binned.py
@@ -12,61 +12,70 @@ import cooler
 from cooler import ice
 
 
+def set_postmortem_hook():
+    import sys, traceback, ipdb
+    def _excepthook(exc_type, value, tb):
+        traceback.print_exception(exc_type, value, tb)
+        print()
+        ipdb.pm()
+    sys.excepthook = _excepthook
+set_postmortem_hook()
+
+
 def main():
-    parser = argparse.ArgumentParser(description="""
+    parser = argparse.ArgumentParser(description="""python matrix_storage_benchmark.py matrix_tsv""")
+    parser.add_argument('matrix_tsv')
+    parser.add_argument(
+        '-s', '--square',
+        default=256,
+        type=int,
+        help="The size of the square within which to return values")
+    parser.add_argument(
+        '-i', '--iterations',
+        default=100,
+        type=int,
+        help="The number of times to run the range query")
 
-    python matrix_storage_benchmark.py matrix_tsv
-""")
+    args = vars(parser.parse_args())
+    binsize = 5000
 
-    parser.add_argument('matrix_tsv', nargs=1)
-    parser.add_argument('-s', '--square', default=256,
-                                        help="The size of the square within which to return values", type=int)
-    parser.add_argument('-i', '--iterations', default=100,
-                                        help="The number of times to run the range query", type=int)
-    args = parser.parse_args()
-    infilepath = args.matrix_tsv[0]
-    outfilepath = op.join(op.dirname(infilepath), 'test-binned.cool')
-
-
-    # Fetch hg19 chromsizes from UCSC
-    chromsizes = cooler.read_chromsizes('http://hgdownload.cse.ucsc.edu/goldenPath/hg19/database/chromInfo.txt.gz')
+    infilepath = args['matrix_tsv']
+    outfilepath = op.join(op.dirname(infilepath), 'chrX.{}kb.cool'.format(binsize//1000))
 
 
     # Build "index"
     t1 = time.time()
-    chromsizes = chromsizes.loc['chrX':'chrX']
+    chromsizes = cooler.read_chromsizes('test/data/hg19.chrom.sizes')
     chroms = ['chrX']
     lengths = [chromsizes['chrX']]
-    binsize = 5000
-    bins = cooler.binnify(chromsizes, binsize)
-    chunksize = 10000000
+    bins = cooler.binnify(chromsizes.loc['chrX':'chrX'], binsize)
+    chunksize = int(100e6)
     reader = cooler.io.SparseLoader(infilepath, chunksize)
-
+    h5opts = dict(compression='gzip', compression_opts=6)
     with h5py.File(outfilepath, 'w') as h5:
-        h5opts = dict(compression='gzip', compression_opts=6)
         cooler.io.create(h5, chroms, lengths, bins, reader, binsize, h5opts=h5opts)
 
     c = cooler.Cooler(outfilepath)
-    matrix = c.matrix()
-    print("Time creating index: {:.2f} seconds".format(time.time() - t1))
+    print("Time creating index: {:.3f} seconds".format(time.time() - t1))
 
 
     # Normalization
     t15 = time.time()
-    N_CPUS = 5
+    N_CPUS = 8
     chunksize = int(100e6)
-
     with h5py.File(outfilepath, 'r+') as h5, Pool(N_CPUS) as pool:
         bias = ice.iterative_correction(
-            h5, chunksize=chunksize, tol=1e-05, min_nnz=10,
+            h5, chunksize=chunksize, tol=1e-05, mad_max=3,
             cis_only=False, ignore_diags=3, map=pool.map)
 
         h5opts = dict(compression='gzip', compression_opts=6)
         h5['bins'].create_dataset('weight', data=bias, **h5opts)
-    print("Time for normalization: {:.2f} seconds".format(time.time() - t15))
+    print("Time for normalization (cis and trans): {:.3f} seconds".format(time.time() - t15))
 
 
     # The bounds of the contact coordinates
+    c = cooler.Cooler(outfilepath)
+    matrix = c.matrix()
     min_x = 0
     min_y = 0
     max_x = c.shape[0]
@@ -76,51 +85,74 @@ def main():
 
 
     # Range queries
-    square_size = args.square
+    square_size = args['square']
     t2 = time.time()
 
-    for i in range(args.iterations):
+    for i in range(args['iterations']):
         point1 = random.randint(min_x, max_x - square_size)
         point2 = random.randint(min_y, max_y - square_size)
         mat = matrix[point1 : point1+square_size, point2 : point2+square_size]
         selected_points = list(zip(mat.row, mat.col, mat.data))
 
     t25 = time.time()
-    print("Time performing range queries (256x256): {:.2f} seconds (per query): {:.2f} seconds".format(t25 - t2, (t25 - t2) / args.iterations))
+    print("Time performing range queries (256x256): {:.3f} seconds (per query): {:.3f} seconds".format(t25 - t2, (t25 - t2) / args['iterations']))
 
-    for i in range(args.iterations):
+    weights = c.bins()['weight'][:].values
+    for i in range(args['iterations']):
+        point1 = random.randint(min_x, max_x - square_size)
+        point2 = random.randint(min_y, max_y - square_size)
+        mat = matrix[point1 : point1+square_size, point2 : point2+square_size]
+        bias1 = weights[point1:point1+square_size]
+        bias2 = weights[point2:point2+square_size]
+        mat.data = bias1[mat.row] * bias2[mat.col] * mat.data
+        selected_points = list(zip(mat.row, mat.col, mat.data))
+
+    t26 = time.time()
+    print("Time performing range queries (256x256) with balancing: {:.3f} seconds (per query): {:.3f} seconds".format(t26 - t25, (t26 - t25) / args['iterations']))
+
+    for i in range(args['iterations']):
         point1 = random.randint(min_x, max_x - square_size * 8)
         point2 = random.randint(min_y, max_y - square_size * 8)
         mat = matrix[point1 : point1+square_size*8, point2 : point2+square_size*8]
         selected_points = list(zip(mat.row, mat.col, mat.data))
 
     t3 = time.time()
-    print("Time performing range queries (2048 x 2048): {:.2f} seconds (per query): {:.2f} seconds".format(t3 - t25, (t3 - t25) / args.iterations))
+    print("Time performing range queries (2048 x 2048): {:.3f} seconds (per query): {:.3f} seconds".format(t3 - t26, (t3 - t26) / args['iterations']))
 
-    for i in range(args.iterations):
+    weights = c.bins()['weight'][:].values
+    for i in range(args['iterations']):
+        point1 = random.randint(min_x, max_x - square_size * 8)
+        point2 = random.randint(min_y, max_y - square_size * 8)
+        mat = matrix[point1 : point1+square_size*8, point2 : point2+square_size*8]
+        selected_points = list(zip(mat.row, mat.col, mat.data))
+
+    t35 = time.time()
+    print("Time performing range queries (2048 x 2048) with balancing: {:.3f} seconds (per query): {:.3f} seconds".format(t35 - t3, (t35 - t3) / args['iterations']))
+
+    for i in range(args['iterations']):
         point1 = random.randint(min_x, max_x - square_size)
         mat = matrix[point1, :]
         selected_points = list(zip(mat.row, mat.col, mat.data))
 
     t4 = time.time()
-    print("Time slicing across first dimension: {:.2f} seconds (per query): {:.2f} seconds".format(t4 - t3, (t4 - t3) / args.iterations))
+    print("Time slicing across first dimension: {:.3f} seconds (per query): {:.3f} seconds".format(t4 - t35, (t4 - t35) / args['iterations']))
 
-    for i in range(args.iterations):
+    for i in range(args['iterations']):
         point2 = random.randint(min_y, max_y - square_size)
         mat = matrix[:, point2]
         selected_points = list(zip(mat.row, mat.col, mat.data))
 
     t5 = time.time()
-    print("Time slicing across second dimension: {:.2f} seconds (per query): {:.2f} seconds".format(t5 - t4, (t5 - t4) / args.iterations))
+    print("Time slicing across second dimension: {:.3f} seconds (per query): {:.3f} seconds".format(t5 - t4, (t5 - t4) / args['iterations']))
 
     selected_points = []
-    for i in range(args.iterations):
-        for pix in c.pixels().iterchunks(size=100000):
+    for i in range(args['iterations']):
+        for pix in c.pixels().iterchunks(size=1000000):
             diag = pix[pix.bin1_id == pix.bin2_id]
             selected_points.extend( list(zip(diag['bin1_id'], diag['bin2_id'], diag['count'])) )
 
     t6 = time.time()
-    print("Time slicing across the diagonal: {:.2f} seconds (per query): {:.2f} seconds".format(t6 - t5, (t6 - t5) / args.iterations))
+    print("Time slicing across the diagonal: {:.3f} seconds (per query): {:.3f} seconds".format(t6 - t5, (t6 - t5) / args['iterations']))
 
 
     # Dump
@@ -128,7 +160,7 @@ def main():
     with open('/tmp/tmp.tsv', 'wt') as f:
         for pix in c.pixels().iterchunks(size=100000):
             pix.to_csv(f, sep='\t', index=False, header=False)
-    print("Time outputting the index: {:.2f}".format(time.time() - t6))
+    print("Time outputting the index: {:.3f}".format(time.time() - t6))
     print("Size of output: {} bytes".format(op.getsize('/tmp/tmp.tsv')))
 
 
--- scripts/cooler_pairs.py
+++ scripts/cooler_pairs.py
@@ -13,46 +13,59 @@ import cooler
 from cooler import ice
 
 
+def set_postmortem_hook():
+    import sys, traceback, ipdb
+    def _excepthook(exc_type, value, tb):
+        traceback.print_exception(exc_type, value, tb)
+        print()
+        ipdb.pm()
+    sys.excepthook = _excepthook
+set_postmortem_hook()
+
+
 def main():
-    parser = argparse.ArgumentParser(description="""
+    parser = argparse.ArgumentParser(
+        description="""python matrix_storage_benchmark.py contacts_txt""")
+    parser.add_argument('contacts_txt')
+    parser.add_argument(
+        '-s', '--square',
+        type=int,
+        default=256,
+        help="The size of the square within which to return values")
+    parser.add_argument(
+        '-i', '--iterations',
+        default=100,
+        type=int,
+        help="The number of times to run the range query")
+    parser.add_argument(
+        '-b', '--binsize',
+        type=int,
+        default=5000,
+        help="The bin resolution in bp")
 
-    python matrix_storage_benchmark.py matrix_tsv
-""")
-
-    parser.add_argument('matrix_tsv', nargs=1)
-    parser.add_argument('-s', '--square', default=256,
-                                        help="The size of the square within which to return values", type=int)
-    parser.add_argument('-i', '--iterations', default=100,
-                                        help="The number of times to run the range query", type=int)
-    args = parser.parse_args()
-    infilepath = args.matrix_tsv[0]
-    outfilepath = op.join(op.dirname(infilepath), 'test-pairs.cool')
-
+    args = vars(parser.parse_args())
+    binsize = args['binsize']
+    infilepath = args['contacts_txt']
+    outfilepath = op.join(op.dirname(infilepath), 'GM12878-MboI.{}kb.cool'.format(binsize // 1000))
 
     # Build "index"
     t1 = time.time()
-    # Bin table
-    chromsizes = cooler.read_chromsizes('test/data/b37.chrom.sizes',
+    chromsizes = cooler.read_chromsizes(
+        'test/data/b37.chrom.sizes',
         name_patterns=[r'^[0-9]+$', r'^[XY]$', r'^MT$'])
-    bins = pd.read_csv(args['bins'], sep='\t', names=['chrom', 'start', 'end'], dtype={'chrom': str})
-    chroms, lengths = list(six.iteritems(chromsizes))
-
-    # Load the binned contacts
-    chunksize = int(100e6)
+    bins = cooler.binnify(chromsizes, binsize)
+    chroms, lengths = zip(*six.iteritems(chromsizes))
     reader = cooler.io.TabixAggregator(infilepath, chromsizes, bins)
     with h5py.File(outfilepath, 'w') as h5:
-        cooler.io.create(h5, chroms, lengths, bins, reader, assembly='b37') # metadata, assembly)
+       cooler.io.create(h5, chroms, lengths, bins, reader, assembly='b37') # metadata)
 
     c = cooler.Cooler(outfilepath)
-    matrix = c.matrix()
-    print("Time creating index: {:.2f} seconds".format(time.time() - t1))
-
+    print("Time creating index: {:.3f} seconds".format(time.time() - t1))
 
     # Normalization
     t15 = time.time()
-    N_CPUS = 5
+    N_CPUS = 8
     chunksize = int(100e6)
-
     with h5py.File(outfilepath, 'r+') as h5, Pool(N_CPUS) as pool:
         bias = ice.iterative_correction(
             h5, chunksize=chunksize, tol=1e-05, mad_max=3,
@@ -60,10 +73,12 @@ def main():
 
         h5opts = dict(compression='gzip', compression_opts=6)
         h5['bins'].create_dataset('weight', data=bias, **h5opts)
-    print("Time for normalization: {:.2f} seconds".format(time.time() - t15))
+    print("Time for normalization (cis and trans): {:.3f} seconds".format(time.time() - t15))
 
 
     # The bounds of the contact coordinates
+    c = cooler.Cooler(outfilepath)
+    matrix = c.matrix()
     min_x = 0
     min_y = 0
     max_x = c.shape[0]
@@ -73,59 +88,82 @@ def main():
 
 
     # Range queries
-    square_size = args.square
+    square_size = args['square']
     t2 = time.time()
 
-    for i in range(args.iterations):
+    for i in range(args['iterations']):
         point1 = random.randint(min_x, max_x - square_size)
         point2 = random.randint(min_y, max_y - square_size)
         mat = matrix[point1 : point1+square_size, point2 : point2+square_size]
         selected_points = list(zip(mat.row, mat.col, mat.data))
 
     t25 = time.time()
-    print("Time performing range queries (256x256): {:.2f} seconds (per query): {:.2f} seconds".format(t25 - t2, (t25 - t2) / args.iterations))
+    print("Time performing range queries (256x256): {:.3f} seconds (per query): {:.3f} seconds".format(t25 - t2, (t25 - t2) / args['iterations']))
 
-    for i in range(args.iterations):
+    weights = c.bins()['weight'][:].values
+    for i in range(args['iterations']):
+        point1 = random.randint(min_x, max_x - square_size)
+        point2 = random.randint(min_y, max_y - square_size)
+        mat = matrix[point1 : point1+square_size, point2 : point2+square_size]
+        bias1 = weights[point1:point1+square_size]
+        bias2 = weights[point2:point2+square_size]
+        mat.data = bias1[mat.row] * bias2[mat.col] * mat.data
+        selected_points = list(zip(mat.row, mat.col, mat.data))
+
+    t26 = time.time()
+    print("Time performing range queries (256x256) with balancing: {:.3f} seconds (per query): {:.3f} seconds".format(t26 - t25, (t26 - t25) / args['iterations']))
+
+    for i in range(args['iterations']):
         point1 = random.randint(min_x, max_x - square_size * 8)
         point2 = random.randint(min_y, max_y - square_size * 8)
         mat = matrix[point1 : point1+square_size*8, point2 : point2+square_size*8]
         selected_points = list(zip(mat.row, mat.col, mat.data))
 
     t3 = time.time()
-    print("Time performing range queries (2048 x 2048): {:.2f} seconds (per query): {:.2f} seconds".format(t3 - t25, (t3 - t25) / args.iterations))
+    print("Time performing range queries (2048 x 2048): {:.3f} seconds (per query): {:.3f} seconds".format(t3 - t26, (t3 - t26) / args['iterations']))
 
-    for i in range(args.iterations):
+    weights = c.bins()['weight'][:].values
+    for i in range(args['iterations']):
+        point1 = random.randint(min_x, max_x - square_size * 8)
+        point2 = random.randint(min_y, max_y - square_size * 8)
+        mat = matrix[point1 : point1+square_size*8, point2 : point2+square_size*8]
+        selected_points = list(zip(mat.row, mat.col, mat.data))
+
+    t35 = time.time()
+    print("Time performing range queries (2048 x 2048) with balancing: {:.3f} seconds (per query): {:.3f} seconds".format(t35 - t3, (t35 - t3) / args['iterations']))
+
+    for i in range(args['iterations']):
         point1 = random.randint(min_x, max_x - square_size)
         mat = matrix[point1, :]
         selected_points = list(zip(mat.row, mat.col, mat.data))
 
     t4 = time.time()
-    print("Time slicing across first dimension: {:.2f} seconds (per query): {:.2f} seconds".format(t4 - t3, (t4 - t3) / args.iterations))
+    print("Time slicing across first dimension: {:.3f} seconds (per query): {:.3f} seconds".format(t4 - t35, (t4 - t35) / args['iterations']))
 
-    for i in range(args.iterations):
+    for i in range(args['iterations']):
         point2 = random.randint(min_y, max_y - square_size)
         mat = matrix[:, point2]
         selected_points = list(zip(mat.row, mat.col, mat.data))
 
     t5 = time.time()
-    print("Time slicing across second dimension: {:.2f} seconds (per query): {:.2f} seconds".format(t5 - t4, (t5 - t4) / args.iterations))
+    print("Time slicing across second dimension: {:.3f} seconds (per query): {:.3f} seconds".format(t5 - t4, (t5 - t4) / args['iterations']))
 
     selected_points = []
-    for i in range(args.iterations):
-        for pix in c.pixels().iterchunks(size=100000):
+    for i in range(args['iterations']):
+        for pix in c.pixels().iterchunks(size=1000000):
             diag = pix[pix.bin1_id == pix.bin2_id]
             selected_points.extend( list(zip(diag['bin1_id'], diag['bin2_id'], diag['count'])) )
 
     t6 = time.time()
-    print("Time slicing across the diagonal: {:.2f} seconds (per query): {:.2f} seconds".format(t6 - t5, (t6 - t5) / args.iterations))
+    print("Time slicing across the diagonal: {:.3f} seconds (per query): {:.3f} seconds".format(t6 - t5, (t6 - t5) / args['iterations']))
 
 
     # Dump
     print("Size of index: {} bytes".format(op.getsize(outfilepath)))
     with open('/tmp/tmp.tsv', 'wt') as f:
-        for pix in c.pixels().iterchunks(size=100000):
+        for pix in c.pixels().iterchunks(size=1000000):
             pix.to_csv(f, sep='\t', index=False, header=False)
-    print("Time outputting the index: {:.2f}".format(time.time() - t6))
+    print("Time outputting the index: {:.3f}".format(time.time() - t6))
     print("Size of output: {} bytes".format(op.getsize('/tmp/tmp.tsv')))
 
 
